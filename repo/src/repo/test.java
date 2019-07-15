b20a605fd7d59fccc4c885a016d770ef515cc314
zarodrig <zarodrig@zone33-wd.cs.ucalgary.ca>
2019-07-15 17:12:02 -0600

    Add a new output message

--- repo/src/repo/test.java
+++ repo/src/repo/test.java
@@ -2,10 +2,12 @@ package repo;
 
 public class test {
 
-	
-	
 	public static void main (String[] args) {
+		
+	int var = 3	;
+		
 	System.out.println("This is a test");
+	System.out.print("One Two Three " + var);
 	}
 	
 }
